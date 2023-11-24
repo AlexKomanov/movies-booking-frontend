7191cc3ff7447c2d83cd938e8755d076edec4395
AlexKomanov <akomanov88@gmail.com>
2023-11-24 15:18:12 +0200

    adding home slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import HomeSlider from '@/components/HomeSlider/HomeSlider'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <HomeSlider />
 
     </main>
   )
---
+++ src/components/HomeSlider/HomeSlider.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+const HomeSlider = () => {
+    return (
+        <Swiper
+            cssMode={true}
+            navigation={true}
+            pagination={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            className="mySwiper"
+        >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+        </Swiper>
+    )
+}
+
+export default HomeSlider
